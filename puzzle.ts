4ca7832b08c2efbda2b3c5f8b3ab0c8111f2e5f8
Dan Bock <danbock@gmail.com>
2024-11-12 07:15:14 -0500

    Use shuf

--- puzzle.ts
+++ puzzle.ts
@@ -1,5 +1,3 @@
-import * as fs from 'fs'
-import * as readline from 'readline'
 import {exec} from 'child_process'
 
 const filePath = process.argv[2]
@@ -9,36 +7,15 @@ if (!filePath) {
   process.exit(1)
 }
 
-const getLineCount = (filePath: string): Promise<number> =>
+const getRandomLine = (filePath: string) =>
   new Promise((resolve, reject) => {
-    exec(`wc -l < ${filePath}`, (error, stdout, stderr) => {
+    exec(`shuf -n 1 ${filePath}`, (error, stdout, stderr) => {
       if (error) {
         reject(`Error: ${stderr}`)
       } else {
-        resolve(parseInt(stdout.trim(), 10))
+        resolve(stdout.trim())
       }
     })
   })
 
-const readRandomLine = async () => {
-  const numLines = await getLineCount(filePath)
-  const randomLineNumber = Math.floor(Math.random() * numLines)
-
-  const fileStream = fs.createReadStream(filePath)
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  })
-
-  let currentLine = 0
-  for await (const line of rl) {
-    console.log({currentLine, line})
-    if (currentLine === randomLineNumber) {
-      console.log('found line')
-      return line
-    }
-    currentLine++
-  }
-}
-
-readRandomLine().then(console.log).catch(console.error)
+getRandomLine(filePath).then(console.log).catch(console.error)
