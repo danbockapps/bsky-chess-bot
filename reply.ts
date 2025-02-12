a4d3cc9b752e78aa8b181edb3882d1f0caaa8aae
Dan Bock <danbock@gmail.com>
2025-02-12 06:27:48 -0500

    Add error handling in reply.ts

--- reply.ts
+++ reply.ts
@@ -6,9 +6,15 @@ import {deepPrint} from './utils'
 const reply = async (reply: ReplyRef, text: string) => {
   const agent = await getAgent()
   const post: AppBskyFeedPost.Record = {text, createdAt: new Date().toISOString(), reply}
-  const result = await agent.post(post)
-  deepPrint(post)
-  deepPrint(result)
+
+  try {
+    const result = await agent.post(post)
+    deepPrint(post)
+    deepPrint(result)
+  } catch (e) {
+    console.error('Error posting reply', new Date().toISOString())
+    console.error(e)
+  }
 }
 
 export default reply
