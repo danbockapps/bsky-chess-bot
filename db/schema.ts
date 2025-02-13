9165d7d39e8ec2d5e75c71a74c34230dc0eab6c1
Dan Bock <danbock@gmail.com>
2025-02-13 07:29:13 -0500

    Add marks view

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,5 @@
-import {int, sqliteTable, text} from 'drizzle-orm/sqlite-core'
+import {aliasedTable, eq, max} from 'drizzle-orm'
+import {int, sqliteTable, sqliteView, text} from 'drizzle-orm/sqlite-core'
 
 // Apply changes to the database: npx drizzle-kit push
 
@@ -15,3 +16,18 @@ export const postsTable = sqliteTable('posts', {
   reply_to_cid: text(),
   fen: text(),
 })
+
+const replies = aliasedTable(postsTable, 'replies')
+
+export const marksView = sqliteView('marks').as((qb) =>
+  qb
+    .select({
+      uri: replies.reply_to_uri,
+      opDate: postsTable.createdAt,
+      username: replies.username,
+      mark: max(replies.correct).as('mark'),
+    })
+    .from(replies)
+    .innerJoin(postsTable, eq(replies.reply_to_uri, postsTable.uri))
+    .groupBy(replies.reply_to_uri, replies.username),
+)
